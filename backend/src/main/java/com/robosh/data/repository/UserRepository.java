6baa733511bb8fbe396ab0c57b503ae62ff4b365
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-10-29 21:20:37 +0200

    create AddressRepository and add @Repository annotation to UserRepository

--- backend/src/main/java/com/robosh/data/repository/UserRepository.java
+++ backend/src/main/java/com/robosh/data/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.robosh.data.repository;
 import com.robosh.data.entity.User;
 import com.robosh.data.enumeration.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLogin(String login);
     List<User> findByRole(Role role);
