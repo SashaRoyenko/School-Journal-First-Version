df01ee911bbb2d8e3b92cd78ec255f2746c3f508
bort1k <nikita.bortnichuk41@gmail.com>
2019-11-09 18:41:42 +0200

    Added sign in page

---
+++ frontend/src/components/SignIn.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Col from "react-bootstrap/Col";
+import App from "../App"
+
+
+const SignIn = () => {
+
+    return (
+        <Container>
+            <Row>
+                <Col lg={{ span: 4, offset: 4 }} style={{marginTop: 150}}>
+                    <h1>
+                        Sign In
+                    </h1>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Login</Form.Label>
+                            <Form.Control type="text" placeholder="Enter login"/>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password"/>
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
+
+}
+
+export default SignIn;
--- frontend/src/components/admin/AddTeacher.jsx
+++ frontend/src/components/admin/AddTeacher.jsx
@@ -128,5 +128,4 @@ const AddTeacher = () => {
     );
 }
 
-
 export default AddTeacher;
