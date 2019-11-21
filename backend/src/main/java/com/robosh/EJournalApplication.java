5a9f2434b44cc1a1eb8d22f2783ae8b0aa7ce21c
Oleksandr Roienko <sasharoyenko@gmail.com>
2019-11-21 13:56:58 +0200

    Add Swagger and change properties

--- backend/src/main/java/com/robosh/EJournalApplication.java
+++ backend/src/main/java/com/robosh/EJournalApplication.java
@@ -2,8 +2,10 @@ package com.robosh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EJournalApplication {
     public static void main(String[] args) {
         SpringApplication.run(EJournalApplication.class, args);
