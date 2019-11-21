dabd9ece930e3145b43f474693785573a6a91c0e
SashaRoyenko <sasharoyenko@gmail.com>
2019-11-21 14:26:57 +0200

    Add ability to get student subjects

--- backend/src/main/java/com/robosh/controller/StudentController.java
+++ backend/src/main/java/com/robosh/controller/StudentController.java
@@ -2,8 +2,10 @@ package com.robosh.controller;
 
 import com.robosh.data.dto.HomeworkDto;
 import com.robosh.data.dto.ScheduleDto;
+import com.robosh.data.dto.SubjectDto;
 import com.robosh.service.HomeworkService;
 import com.robosh.service.ScheduleService;
+import com.robosh.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,7 @@ public class StudentController {
 
     private HomeworkService homeworkService;
     private ScheduleService scheduleService;
+    private SubjectService subjectService;
 
     @Autowired
     public StudentController(HomeworkService homeworkService, ScheduleService scheduleService) {
@@ -31,13 +34,18 @@ public class StudentController {
 //    }
 
     @GetMapping("/{studentId}/subject/{subjectId}/homework")
-    public List<HomeworkDto> getHomework(@PathVariable Long studentId, @PathVariable Long subjectId) {
+    public List<HomeworkDto> getHomework(@PathVariable("studentId") Long studentId, @PathVariable("subjectId") Long subjectId) {
         return homeworkService.findByUserIdAndSubjectId(studentId, subjectId);
     }
 
-    @GetMapping("schedule/group/{Id}")
+    @GetMapping("schedule/group/{id}")
     public List<ScheduleDto> getSchedule(@PathVariable Long id) {
         return scheduleService.findByGroupId(id);
+    }
+
+    @GetMapping("/group/{id}/subject")
+    public List<SubjectDto> getStudentSubjects(@PathVariable("id") Long id){
+        return subjectService.findSubjectsByGroupId(id);
     }
 
 }
