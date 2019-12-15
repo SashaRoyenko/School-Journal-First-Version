9b2df158c5a96113fb3a1eb3e71663ff6ad04a05
Orest Shemeliuk <orestshemelyuk@gmail.com>
2019-12-15 18:39:24 +0200

    remove login from constructor parameters and optimized imports

--- backend/src/main/java/com/robosh/data/entity/Teacher.java
+++ backend/src/main/java/com/robosh/data/entity/Teacher.java
@@ -3,11 +3,8 @@ package com.robosh.data.entity;
 import com.robosh.data.enumeration.Role;
 import lombok.*;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 
 @EqualsAndHashCode(callSuper = true)
@@ -21,8 +18,7 @@ public class Teacher extends User {
 
     @Builder(builderMethodName = "teacherBuilder")
     public Teacher(Long id, String firstName,
-                   String secondName, String lastName,
-                   String login, String password,
+                   String secondName, String lastName, String password,
                    String email, String phone, Role role,
                    String address, Boolean active) {
         super(id, firstName, secondName, lastName, password, email, phone, role, active);
