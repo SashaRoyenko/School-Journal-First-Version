8112a673cc088a28bcad3a64e01394c6d23605a2
Orest Shemeliuk <orestshemelyuk@gmail.com>
2019-12-15 17:57:52 +0200

    create admin init function

--- backend/src/main/java/com/robosh/controller/MainController.java
+++ backend/src/main/java/com/robosh/controller/MainController.java
@@ -4,6 +4,7 @@ import com.robosh.data.dto.SubjectDto;
 import com.robosh.data.dto.TeacherDto;
 import com.robosh.data.entity.User;
 import com.robosh.data.enumeration.Role;
+import com.robosh.service.StudentService;
 import com.robosh.service.SubjectService;
 import com.robosh.service.TeacherService;
 import com.robosh.service.UserService;
@@ -23,12 +24,14 @@ public class MainController {
     private UserService userService;
     private SubjectService subjectService;
     private TeacherService teacherService;
+    private StudentService studentService;
 
     @Autowired
-    public MainController(UserService userService, SubjectService subjectService, TeacherService teacherService) {
+    public MainController(UserService userService, SubjectService subjectService, TeacherService teacherService, StudentService studentService) {
         this.userService = userService;
         this.subjectService = subjectService;
         this.teacherService = teacherService;
+        this.studentService = studentService;
     }
 
 
@@ -50,6 +53,54 @@ public class MainController {
         user.setRole(Role.ADMIN);
 
         userService.saveUser(user);
+    }
+
+    @PostMapping("/init")
+    public void initDb() {
+        initAdmins();
+    }
+
+    private void initAdmins() {
+        User admin1 = new User();
+        admin1.setFirstName("Орест");
+        admin1.setSecondName("Андрійович");
+        admin1.setLastName("Шемелюк");
+        admin1.setEmail("orest_shemelyuk@gmail.com");
+        admin1.setPhone("+380552827486");
+        admin1.setPassword("admin1");
+        admin1.setRole(Role.ADMIN);
+
+        User admin2 = new User();
+        admin2.setFirstName("Олександр");
+        admin2.setSecondName("Олегович");
+        admin2.setLastName("Роєнко");
+        admin2.setEmail("oleks_roienko@gmail.com");
+        admin2.setPhone("+380222827486");
+        admin2.setPassword("admin2");
+        admin2.setRole(Role.ADMIN);
+
+        User admin3 = new User();
+        admin3.setFirstName("Констянтин");
+        admin3.setSecondName("Миколайович");
+        admin3.setLastName("Сторожук");
+        admin3.setEmail("kostia_stotozhuk@gmail.com");
+        admin3.setPhone("+380112827486");
+        admin3.setPassword("admin3");
+        admin3.setRole(Role.ADMIN);
+
+        User admin4 = new User();
+        admin4.setFirstName("Нікіта");
+        admin4.setSecondName("Васильович");
+        admin4.setLastName("Бортнічук");
+        admin4.setEmail("nikita_bortnichuk@gmail.com");
+        admin4.setPhone("+380992827486");
+        admin4.setPassword("admin4");
+        admin4.setRole(Role.ADMIN);
+
+        userService.saveUser(admin1);
+        userService.saveUser(admin2);
+        userService.saveUser(admin3);
+        userService.saveUser(admin4);
     }
 
     @GetMapping("/subjects/{id}")
