ee23a685e8a37e04e092f532fd064ae6ec1ee85d
Ostap <orestshemelyuk@gmail.com>
2019-12-27 20:20:03 +0200

    divide html profile into parts

--- backend/src/main/java/com/robosh/controller/TeacherController.java
+++ backend/src/main/java/com/robosh/controller/TeacherController.java
@@ -1,12 +1,19 @@
 package com.robosh.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static com.robosh.common_routes.Routes.TEACHER_MAPPING;
 
 @Controller
 @RequestMapping(TEACHER_MAPPING)
+@PreAuthorize("hasAuthority('ADMIN')")
 public class TeacherController {
 
+    @GetMapping("")
+    public String teacherProfile() {
+        return "teacher/profile";
+    }
 }
